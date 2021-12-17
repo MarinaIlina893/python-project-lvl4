39330917235cae238f66c9d4f35cde3ebf17d6dc
MarinaIlina893 <marinailina893@gmail.com>
2021-12-18 01:51:15 +0500

    fix users create url

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UserCreate, UserListView, UserUpdate, UserDelete
 
 urlpatterns = [
-    path('create', UserCreate.as_view(), name='users/create/'),
+    path('create/', UserCreate.as_view(), name='users/create/'),
     path('', UserListView.as_view(), name='users'),
     path('<int:pk>/update/', UserUpdate.as_view(), name='users/update'),
     path('<int:pk>/delete/', UserDelete.as_view(), name='users/delete')
