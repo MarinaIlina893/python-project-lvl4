a8f42bdb219817f61a6888488cfcc1a1bc6bb15f
MarinaIlina893 <marinailina893@gmail.com>
2022-01-22 22:31:24 +0500

    fix debug value

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG_VALUE', False)
 
-ALLOWED_HOSTS = ['minimalistictaskmanager.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','minimalistictaskmanager.herokuapp.com']
 
 
 # Application definition
